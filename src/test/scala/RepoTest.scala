c852ae4716b4bc224af4294e3057376cf6643a13
Ruslan Banakh <ruslan.banakh@scoutgaminggroup.com>
2019-05-09 19:10:48 +0300

    Implement Tests

--- src/test/scala/RepoTest.scala
+++ src/test/scala/RepoTest.scala
@@ -1,11 +1,41 @@
 import org.scalatest._
+import Matchers._
 
 class RepoTest extends FlatSpec {
-  val userRep = new UserRep()
-  val deviceRep = new UserRep()
+  val idUserRepository = new IdUserRep()
+  val idDeviceRepository =  new IdDeviceRep()
 
-//  it should "return positive result" in {
-//    a.validate shouldEqual Right(a)
-//  }
+  val futureUserRepository = new FutureUserRep()
+  val futureDeviceRepository =  new FutureDeviceRep()
+
+  val users = List("Bob", "Jon", "Ava")
+  val devices = List("device1", "device2", "device3")
+
+  val userService = new UserService(idUserRepository)
+  val deviceService = new IotDeviceService(idDeviceRepository, idUserRepository)
+
+  it should s"Created Users $users" in {
+    users.map(user => userService.registerUser(user))
+    users.map(userName => userService.getByUsername(userName).fold("")(user => user.username) shouldEqual userName)
+    users.zipWithIndex.foreach{ case (userName, i) => userService.getById(i).fold("")(user => user.username) shouldEqual userName }
+  }
+
+  it should s"Created Users with exist userName" in {
+    users.map(user => userService.registerUser(user) should be ('left))
+  }
+
+  it should s"Created Devices $devices" in {
+    users.zipWithIndex.foreach{ case (_, userId) => {
+      devices.map(deviceSn => deviceService.registerDevice(userId, s"$userId-$deviceSn") should be ('right))
+    }
+    }
+  }
+
+  it should s"Created Devices with exist serial number" in {
+    users.zipWithIndex.foreach{ case (_, userId) => {
+      devices.map(deviceSn => deviceService.registerDevice(userId, s"$userId-$deviceSn") should be ('left))
+    }
+    }
+  }
 
 }
